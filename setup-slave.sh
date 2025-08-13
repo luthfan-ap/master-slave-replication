375ab603389e8996e91332aea99a52821b83d723
luthfan-ap <luthfan@gmail.com>
2025-08-13 16:50:27 +0700

    Major fix: gatau apa aja tadi wkwkwk

--- setup-slave.sh
+++ setup-slave.sh
@@ -3,22 +3,18 @@
 # setup-slave.sh
 # This script configures a PostgreSQL slave to connect to the master for replication.
 
-# Wait for the master database to be available.
-until pg_isready -h postgres-master -p 5432 -U user; do
+# Wait for the master to be ready
+until pg_isready -h postgres-master -p 5432 -U "$PG_USER"; do
   echo "Waiting for postgres-master..."
   sleep 1
 done
 echo "postgres-master is up and running."
 
-# Create the recovery.conf file to enable replication
-echo "standby_mode = 'on'" > /var/lib/postgresql/data/postgresql.conf
-echo "primary_conninfo = 'host=postgres-master port=5432 user=replication_user password=password application_name=postgres-slave-1'" >> /var/lib/postgresql/data/postgresql.conf
-echo "hot_standby = 'on'" >> /var/lib/postgresql/data/postgresql.conf
-echo "recovery_target_timeline = 'latest'" >> /var/lib/postgresql/data/postgresql.conf
+# Clean old data directory
+rm -rf /var/lib/postgresql/data/*
 
-# Use pg_basebackup to copy the master's data directory.
-# This must be run as the 'postgres' user.
-su - postgres -c "pg_basebackup -h postgres-master -p 5432 -U replication_user -D /var/lib/postgresql/data -P -Xs -R"
+# Run base backup as 'postgres' user
+su - postgres -c "pg_basebackup -h postgres-master -p 5432 -U "$PG_USER" -D /var/lib/postgresql/data -P -Xs -R"
 
-# Start the PostgreSQL slave instance.
-su - postgres -c "postgres"
+# The -R flag creates standby.signal and sets primary_conninfo automatically
+# Do not start postgres manually; let the container entrypoint handle it
